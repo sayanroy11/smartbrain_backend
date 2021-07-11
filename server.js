bd730358a25e14b29dd91e9825a0ad853abe02e6
sayanroy11 <85430745+sayanroy11@users.noreply.github.com>
2021-07-11 22:32:44 +0530

    server added

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const image = require('./controllers/image');
 const db=knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'rith10',
-    database : 'smart_brain'
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
   }
 });
 
